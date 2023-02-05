5ae0538b650f220897d73d4d8d6bc6fc9fb298ee
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-05 14:11:07 -0600

    add contacts route

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', (req, res, next) => {
+  res.send(`ID: ${req.params.id}`)
+})
+
 module.exports = router;
