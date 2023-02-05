92a62695bcfacb8243b3902cb86c08f99402b6f4
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-05 15:44:29 -0600

    add checks for form submission

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -29,6 +29,22 @@ router.get('/add', (req, res, next) => {
   res.render('contactForm', {title: "Contact Form"})
 })
 
+/* POST create contact form */
+router.post('/add', (req, res, next) => {
+  let newContact = {
+    firstName: req.body.firstName.trim(),
+    lastName: req.body.lastName.trim(),
+    email: req.body.email.trim(),
+    notes: req.body.notes.trim(),
+  }
+  if (newContact.firstName === '' || newContact.lastName === '' || newContact.notes === ''){
+    res.render('contactForm', {title: "Contacts Form", msg: "Form fields cannot be empty!"})
+  } else {
+    res.send("hi")
+  }
+  //res.render('contactForm', {title: "Contact Form"})
+})
+
 /* GET contactID listing */
 router.get('/:id', (req, res, next) => {
   let id = data.find(contact => contact.contactID == req.params.id)
