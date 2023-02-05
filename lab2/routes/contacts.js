2cdff312273946b3579f9eb74a509ea2698a98bd
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-05 14:29:26 -0600

    display dummy data

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let data = [
+  {
+    contactID: 2,
+    firstName: "John",
+    lastName: "Smith",
+    emailAddress: "random@email.com",
+    notes: "test",
+    date: Date()
+   },
+  {
+    contactID: 3,
+    firstName: "Jane",
+    lastName: "Smith",
+    emailAddress: "random@email.com",
+    notes: "best",
+    date: Date()
+  }
+]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('contacts', {title: "Contacts", contactsList: data});
 });
 
 router.get('/:id', (req, res, next) => {
