bb2d14ca5d042eaa094c137caa7984555c922152
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-05 14:56:19 -0600

    add view and route for form

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -19,14 +19,22 @@ let data = [
     date: Date()
   }
 ]
-/* GET users listing. */
+/* GET contacts listing. */
 router.get('/', function(req, res, next) {
   res.render('contacts', {title: "Contacts", contactsList: data});
 });
 
+/* GET create contact form */
+router.get('/add', (req, res, next) => {
+  res.render('contactForm', {title: "Contact Form"})
+})
+
+/* GET contactID listing */
 router.get('/:id', (req, res, next) => {
   let id = data.find(contact => contact.contactID == req.params.id)
   res.render('contactsID', {title: id.firstName, info:id})
 })
+
+
 
 module.exports = router;
