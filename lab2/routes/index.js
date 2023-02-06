d564b8b0a8e3272607d8977ddb38c94b2ccdece5
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-06 12:26:21 -0600

    add introduction information and my contact

--- lab2/routes/index.js
+++ lab2/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lab 2' });
+  res.render('index', { 
+                        title: 'Contacts List', 
+                        intro: 'This application allows you to keep track of the contact information for different people. The application is served on a Node/Express application and contact information is stored on the file system with JSON. Users can also edit and delete contacts if they so desire.',
+                        myInfo: {
+                                  name: 'Yongkang Hu',
+                                  email: 'yhu78@hawk.iit.edu'
+                                } 
+                      });
 });
 
 module.exports = router;
