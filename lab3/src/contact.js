a963284991f1c515a5f55059d5df5a8a7c7c7795
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 15:23:50 -0600

    add contact class

---
+++ lab3/src/contact.js
@@ -0,0 +1,12 @@
+class contact {
+    constructor(contactID, firstName, lastName, email, notes, date) {
+        this.contactID = contactID
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.notes = notes
+        this.date = date
+    }
+}
+
+module.exports = contact
