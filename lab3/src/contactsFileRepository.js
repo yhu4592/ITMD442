0291697a8d7ea06332dc9981f9c5c078d0cae373
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 15:02:58 -0600

    run createSmt

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -6,6 +6,8 @@ const betterSqlite3 = require('better-sqlite3')
 const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
 
 const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT")
+createSmt.run()
+
 const repo = {
   findAll: ,
   findById: ,
