48c8556559c118b7fd711a5cc1964ad776b96557
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 15:04:25 -0600

    fix syntax errors

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -5,12 +5,12 @@ const betterSqlite3 = require('better-sqlite3')
 
 const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
 
-const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT")
+const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT)")
 createSmt.run()
 
 const repo = {
-  findAll: ,
-  findById: ,
+  findAll: () => {},
+  findById: () => {},
   create: newContact => {
     
   },
