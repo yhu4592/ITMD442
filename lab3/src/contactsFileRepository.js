043ea566401678ab5d594b706df1605281011f98
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 16:02:13 -0600

    update table to use INTEGER key with autoincrement

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -1,4 +1,3 @@
-const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
 const Contact = require('./contact')
@@ -6,7 +5,7 @@ const betterSqlite3 = require('better-sqlite3')
 
 const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
 
-const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT)")
+const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT)")
 createSmt.run()
 
 const repo = {
