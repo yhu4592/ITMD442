65a5b0332d32ed9b01230fe61dbb0e1018945a7c
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 14:34:02 -0600

    clone lab 2

---
+++ lab3/routes/contacts.js
@@ -0,0 +1,76 @@
+var express = require('express')
+var router = express.Router()
+const contactsRepo = require('../src/contactsFileRepository')
+
+/* GET contacts listing. */
+router.get('/', function(req, res, next) {
+  const data = contactsRepo.findAll()
+  res.render('contacts', {title: "Contacts", contactsList: data});
+})
+
+/* GET create contact form */
+router.get('/add', (req, res, next) => {
+  res.render('contactForm', {title: "Contact Form"})
+})
+
+/* POST create contact form */
+router.post('/add', (req, res, next) => {
+  const newContact = {
+    firstName: req.body.firstName.trim(),
+    lastName: req.body.lastName.trim(),
+    email: req.body.email.trim(),
+    notes: req.body.notes.trim(),
+    date: Date()
+  }
+  if (newContact.firstName === '' || newContact.lastName === ''){
+    res.render('contactForm', {title: "Contacts Form", msg: "Name fields cannot be empty!"})
+  } else {
+    contactsRepo.create(newContact)
+    res.redirect('/contacts')
+  }
+})
+
+/* GET contactID listing */
+router.get('/:id', (req, res, next) => {
+  const id = contactsRepo.findById(req.params.id)
+  if (id) {
+    res.render('contactsID', {title: id.firstName + ' ' + id.lastName, info:id})
+  } else {
+    res.redirect('/contacts')
+  }
+})
+
+/* GET delete form */
+router.get('/:id/delete', (req, res, next) => {
+  res.render('contactFormDelete', {title: "Delete Contact", info: contactsRepo.findById(req.params.id)})
+})
+
+/* POST delete contact */
+router.post('/:id/delete', (req, res, next) => {
+  contactsRepo.deleteById(req.params.id)
+  res.redirect('/contacts')
+})
+
+/* GET edit form */
+router.get('/:id/edit', (req, res, next) => {
+  res.render('contactFormEdit', {title: 'Edit Contact', info: contactsRepo.findById(req.params.id)})
+})
+
+router.post('/:id/edit', (req, res, next) => {
+  const updateContact = {
+    contactID: req.params.id,
+    firstName: req.body.firstName.trim(),
+    lastName: req.body.lastName.trim(),
+    email: req.body.email.trim(),
+    notes: req.body.notes.trim(),
+    date: Date()
+  }
+  if (updateContact.firstName === '' || updateContact.lastName === ''){
+    res.render('contactFormEdit', {title: "Contacts Form", info: contactsRepo.findById(req.params.id), msg: "Name fields cannot be empty!"})
+  } else {
+    contactsRepo.update(updateContact)
+    res.redirect(`/contacts/${req.params.id}`)
+  }
+})
+
+module.exports = router;
---
+++ lab3/src/contactsFileRepository.js
@@ -0,0 +1,45 @@
+const db = new Map();
+const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
+
+const loadData = () => {
+  const jsonData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'))
+  const contactsArray = JSON.parse(jsonData)
+  contactsArray.forEach(contact => {
+    db.set(contact[0], contact[1])
+  })
+}
+const saveData = () => {
+  const stringifyData = JSON.stringify(Array.from(db))
+  fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData)
+}
+
+const repo = {
+  findAll: () => Array.from(db.values()),
+  findById: id => db.get(id),
+  create: newContact => {
+    const contact = {
+      contactID: crypto.randomUUID(),
+      firstName: newContact.firstName,
+      lastName: newContact.lastName,
+      email: newContact.email,
+      notes: newContact.notes,
+      date: newContact.date
+    }
+    db.set(contact.contactID, contact)
+    saveData()
+  },
+  deleteById: id => {
+    db.delete(id)
+    saveData()
+  },
+  update: contact => {
+    db.set(contact.contactID, contact)
+    saveData()
+  }
+}
+
+loadData()
+
+module.exports = repo
