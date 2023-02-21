88fd779419e0595bce405991dabb77862b73052c
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 14:59:18 -0600

    add create table statement

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -2,8 +2,10 @@ const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
 const betterSqlite3 = require('better-sqlite3')
+
 const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
 
+const createSmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (contactID TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT, notes TEXT, date TEXT")
 const repo = {
   findAll: ,
   findById: ,
