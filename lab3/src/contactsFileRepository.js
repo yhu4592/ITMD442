00a7f025c3bcb97af9c44d9856044d974d76fda0
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 15:24:19 -0600

    import contact class

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
+const contact = require('./contact')
 const betterSqlite3 = require('better-sqlite3')
 
 const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
