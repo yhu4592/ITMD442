53c230ef0e718cff371ab6ca29971ff57c629250
Yongkang Hu <hu.yongkangsg114@gmail.com>
2023-02-21 14:52:07 -0600

    create sqlite data file

--- lab3/src/contactsFileRepository.js
+++ lab3/src/contactsFileRepository.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto')
 const fs = require('fs')
 const path = require('path')
+const betterSqlite3 = require('better-sqlite3')
+const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log })
 
 const repo = {
   findAll: ,
